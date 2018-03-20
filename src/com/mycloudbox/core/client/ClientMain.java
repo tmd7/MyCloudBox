f48bbcdb65986236b95f6212b121cdc4ba3c1512
Marat Tlepberghenov <mur.marat@gmail.com>
2018-03-21 00:08:13 +0200

    Changed name of client's fxml file. Added RootLayout with BorderPane. Edited structure of UI

--- src/com/mycloudbox/core/client/ClientMain.java
+++ src/com/mycloudbox/core/client/ClientMain.java
@@ -4,18 +4,65 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class ClientMain extends Application {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("ClientView.fxml"));
-        primaryStage.setTitle("My Cloud Box");
-        primaryStage.setScene(new Scene(root, 1000, 800));
-        primaryStage.show();
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("My Cloud Box - Client");
+
+        initRootLayout();
+        showClientLayout();
+
+//
+//        primaryStage.setTitle("My Cloud Box");
+//        primaryStage.setScene(new Scene(root, 1000, 800));
+//        primaryStage.show();
     }
 
+    //Init root layout
+    public void initRootLayout() {
+        try {
+            //Download root layout
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ClientMain.class.getResource("RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            //Puts in primaryStage scene and puts in scene rootLayout
+            //Stage --> Scene --> rootLayout
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showClientLayout() {
+        //Download ClientLayout
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ClientMain.class.getResource("ClientLayout.fxml"));
+            AnchorPane clientLayout = (AnchorPane) loader.load();
+
+            //Puts ClientLayout in center of RootLayout
+            rootLayout.setCenter(clientLayout);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
